de57fe62df9395a9e49b338a0433d3051e240218
Lazaro-Nascimento <lazaronascimentogms@gmail.com>
2026-07-25 22:35:25 -0300

    [fix] (WeatherForecast) - Teste de mudança de valor para rodar CI/CD no GitHub Actions

--- PrimeiraApi/WeatherForecast.cs
+++ PrimeiraApi/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace PrimeiraApi_WeatherForecast
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 20 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
 
